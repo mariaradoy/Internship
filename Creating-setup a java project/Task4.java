0d7a725f79b5df64d77eb57d84dbd2d1fd9e0e11
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-10-12 20:48:55 +0300

    Create Task4.java

---
+++ Creating-setup a java project/Task4.java
@@ -0,0 +1,7 @@
+//Write a Java program to compute the specified expressions and print the output.
+
+public class exmpl {
+    public static void main(String[] args) {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
