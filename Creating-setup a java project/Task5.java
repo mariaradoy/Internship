f61d7b6c4b5bdeb8d251e7e88587ec15e8664f0b
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-10-12 20:54:16 +0300

    Create Task5.java

---
+++ Creating-setup a java project/Task5.java
@@ -0,0 +1,7 @@
+//Write a Java program to compute a specified formula
+
+public class exmpl {
+    public static void main(String[] args) {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
