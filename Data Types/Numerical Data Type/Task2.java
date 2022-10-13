0160a626e7bd50fa5178e7089c649e267ca15a67
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-10-13 10:49:59 +0300

    Create Task2.java

---
+++ Data Types/Numerical Data Type/Task2.java
@@ -0,0 +1,14 @@
+//What will be displayed after the execution of the following sequences?
+
+float x = 1/2;
+System.out.println(x);
+System.out.println(-15/4);
+System.out.println(“ ”);
+System.out.println(-15%4);
+
+//Rwsult
+
+//0.0
+//-3
+// 
+//-3
