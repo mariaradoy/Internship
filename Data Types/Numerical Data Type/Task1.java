762c375eca64cae78d933433d08d8f04b92c621a
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-10-13 10:47:54 +0300

    Create Task1.java

---
+++ Data Types/Numerical Data Type/Task1.java
@@ -0,0 +1,20 @@
+// Print to console one value for each java primitive (one per line)
+
+public class exmpl {
+    public static void main(String[] args) {
+        boolean b = true;
+        char c = 'm';
+        int i = 14;
+        long l = 1451545L;
+        float f = 0.12f;
+        double d = 34.5d;
+
+        System.out.println("boolean b = " + b);
+        System.out.println("char c = " + c);
+        System.out.println("int i = " + i);
+        System.out.println("long l = " + l);
+        System.out.println("float f = " + f);
+        System.out.println("double d = " + d);
+
+    }
+}
