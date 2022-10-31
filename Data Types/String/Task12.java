89d091ae165c7fb10e5f53cd75f50e74e777d94a
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-10-31 12:00:00 +0300

    Create Task12.java

---
+++ Data Types/String/Task12.java
@@ -0,0 +1,8 @@
+public class task12 {
+    public static void main(String[] args) {
+        String str = "my name is";
+        for(int i = str.length()-1; i >=0; i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
