a92a4498f78bff9f6bffeb13473b3298b4916283
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-11-22 14:09:13 +0300

    Create Task5.java

---
+++ Files and input-output streams/Task5.java
@@ -0,0 +1,9 @@
+//Realize a class IOFile that exports some functionalities on text files. The class should have
+//a constructor with one parameter of type String, representing the name of the file on which to operate, and
+//should export the following methods:
+//
+//    int countLines() : that returns the number of lines of the file;
+//    void write(OutputStream os) : that writes the content of the file to os;
+//    void print() : that prints the content of the file to the video;
+//    void copy(String filename) : that copies the content of the file to the file specified by filename;
+//    void delete() : that deletes the file from mass-storage.
