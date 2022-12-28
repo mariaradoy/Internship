37bb97c1887fb7649d1476101d76fa1bb1e32843
mariaradoy <98696375+mariaradoy@users.noreply.github.com>
2022-12-28 16:57:33 +0300

    Update Task9.java

--- Inheritance/Task9.java
+++ Inheritance/Task9.java
@@ -1,3 +1,10 @@
+// Write a program to implement polymorphism using inheritance. 
+// Consider the example of Shape as base class with method show(). 
+// And then a child class Circle and Rectangle which inherit the base class Shape and override its method show(). 
+// Add one more Method with the name of getInfo(). This method would display the class name in which it is implemented. 
+// Do not override this method. When you will call the method getInfo() with child object it would still show the  name of the base class, 
+// which implies that method has been directly inherited and was not overridden.
+
 class Shape{
     public void show(){
         System.out.println("Show shape");
